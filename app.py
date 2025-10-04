e8e47c46a89c96d899400473b95645df56203580
AlanElNufjaja <a01564225@tec.mx>
2025-10-04 16:49:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import streamlit as st
 from parameters import obtener_coordenadas
 from damage import generar_puntos_circulo, calcular_radio_impacto
 from mapa import mostrar_mapa
+import streamlit as st
+from asteroides import lista_ids, datos_meteorito
+
+# Selección del meteorito
+meteorito_seleccionado = st.selectbox("Selecciona un meteorito", lista_ids())
+
+# Obtener datos
+datos = datos_meteorito(meteorito_seleccionado)
+if datos:
+    st.write(datos)
+    # Aquí puedes usar datos['relative_velocity.kilometers_per_second'], datos['estimated_diameter.kilometers.estimated_diameter_max'], etc.
 
 st.set_page_config(layout="wide")
 st.title("Visualizador de Meteoritos 2D ☄️")
