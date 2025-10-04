5c4ec43a83326e3b5269e8639d9aa8652d1f45d5
AlanElNufjaja <a01564225@tec.mx>
2025-10-04 14:07:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.title("Visualizador de Meteoritos 2D ☄️")
 
 # Entrada del usuario
 lugar = st.sidebar.text_input("Nombre de la ciudad")
-lat_manual = st.sidebar.slider("Latitud manual", -90, 90, 100)
-lon_manual = st.sidebar.slider("Longitud manual", -180, 180, 100)
+lat_manual = st.sidebar.slider("Latitud manual", -90, 90, 90)
+lon_manual = st.sidebar.slider("Longitud manual", -180, 180, 90)
 tamano = st.sidebar.slider("Tamaño del meteorito (m)", 10, 500, 100)
 
 # Obtener coordenadas
