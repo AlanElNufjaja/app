5d5a0957dca0265e0171131f6dc69dc6e2eb7dee
AlanElNufjaja <a01564225@tec.mx>
2025-10-04 21:11:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ radio_km = max(radio_km, 0.01)  # asegurar visibilidad en el mapa
 energia_joules = 0.5 * densidad * (4/3 * np.pi * radio_km**3) * velocidad_kms**2
 
 if material == "Roca dura":
-    profundidad_m = diametro_m / 5
+    profundidad_m = radio_km*2 / 5
 
 elif material == "Tierra blanda":
     diametro_m = 0.1 * (energia_joules / (DENSIDAD_ROCA * g))**0.25 * 1.15
