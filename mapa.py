efd39262cfb02e134c121fc0f4b3fc04bd4737d3
AlanElNufjaja <a01564225@tec.mx>
2025-10-05 09:37:36 -0600

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -28,12 +28,22 @@ def mostrar_mapa(df, lat, lon, radio_km, tipodano):
         layers = [capa_negra, capa_rojo]
         
     elif tipodano == "Bola de fuego": 
+         capa_negra = pdk.Layer(
+            "ScatterplotLayer",
+            data=df,
+            get_position=["lon", "lat"],
+            get_color=[255, 0, 0, 150], 
+            get_radius=radio_km*50,
+            pickable=False,
+            get_line_color=[0, 0, 0, 255],  
+            get_line_width=1
+        )
         capa_rojo = pdk.Layer(
             "ScatterplotLayer",
             data=df,
             get_position=["lon", "lat"],
             get_color=[255, 0, 0, 150],
-            get_radius=radio_km*50,
+            get_radius=radio_km*250,
             pickable=False,
             get_line_color=[255, 0, 0, 255],  
             get_line_width=1
