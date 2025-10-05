9b5671393acb045b89894ac39fbe36512a04ee99
AlanElNufjaja <a01564225@tec.mx>
2025-10-04 19:30:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ exp_factor = st.sidebar.slider(
     "Nivel de abrasión atmosférica (potencia de 10)",
     -9.0, -7.5, -9.0, step=0.01
 )
-factor_calor = (10 ** exp_factor)*10
+factor_calor = (10 ** exp_factor)*100
 st.sidebar.write(f"Constante actual: {factor_calor:.1e}")
 # ======================
 # Cálculos principales
