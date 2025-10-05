8b60c1b520a72aa98628afd86c38ec53e293ff23
AlanElNufjaja <a01564225@tec.mx>
2025-10-05 12:37:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -128,17 +128,17 @@ st.markdown(f"""
 </div>
 """, unsafe_allow_html=True)
 
+mostrar_mapa(df, lat, lon, radio_km,tipodano)
+
 st.write("---")
 
 url_externa = "https://appgit-nmfd77m9kjolnscbbubqzg.streamlit.app"
 col_a, col_b, col_c = st.columns([1, 3, 1])
 with col_b:
     st.link_button(
-        "🚀 Go to damage simulator",
+        "Go to damage simulator",
         url_externa,
         use_container_width=True,
         type="primary"
     )
 st.write("---")
-
-mostrar_mapa(df, lat, lon, radio_km,tipodano)
