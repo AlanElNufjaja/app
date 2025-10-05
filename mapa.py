d03c2545632be1cba97d50184b2c339e1062ac4a
AlanElNufjaja <a01564225@tec.mx>
2025-10-05 08:53:53 -0600

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -29,7 +29,7 @@ def mostrar_mapa(df, lat, lon, radio_km, tipodano):
             data=df,
             get_position=["lon", "lat"],
             get_color=[255, 0, 0, 160],
-            get_radius,
+            get_radius=radio_km*12.5,
             pickable=False
         )
         capa_naranja = pdk.Layer(
